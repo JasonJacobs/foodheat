59344dc2f4bb5460e9516ab4de8408e176df83af
jeremysklarsky <jeremy.sklarsky@gmail.com>
2015-03-31 14:00:25 -0400

    pulled out coordinates, define cuisines

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,13 @@
 class RestaurantsController < ApplicationController
 
   def index
+    @cuisines = ["Japanese", "Chinese", "Mexican", "Coffee Shops", "Bakery", "BBQ", "Pizza", "Caribbean", "Italian", "Thai", "Indian", "French", "American / New American", "Southern", "Food Truck", "Greek", "Middle Eastern", "Fast Food"].sort
   end
 
   def search
     @search = Search.new(params[:cuisine])
+    coordinates = @search.get_coordinates(@search.search_for_cuisine)
+    binding.pry
   end
 
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -4,7 +4,6 @@ class Search
 
   def initialize(cuisine)
     @cuisine = cuisine
-    search_for_cuisine
   end
 
   def search_for_cuisine
@@ -13,18 +12,57 @@ class Search
     when "Japanese"
       categories = ["Japanese", "Sushi", "Ramen", "Udon"]
     when "Chinese"
-      categories = ["Chinese", "Cantonese", "Dumplings"]
+      categories = ["Chinese", "Cantonese", "Dumplings"] 
+    when "Mexican"
+      categories = ["Mexican", "Tacos", "Taqueria"]
+    when "Coffee Shops"
+      categories = ["Coffee", "Café"]
+    when "Bakery"
+      categories = ["Bakery", "Bagels"]
+    when "BBQ"
+      categories = ["BBQ"]
+    when "Pizza"
+      categories = ["Pizza"]
+    when "Caribbean"
+      categories = ["Caribbean", "Jamaican", "Dominican"]
+    when "Italian"
+      categories = ["Italian"]
+    when "Thai"
+      categories = ["Thai"]
+    when "Indian"
+      categories = ["Indian"]
+    when "French"
+      categories = ["French"]
+    when "American / New American"
+      categories = ["Diner", "American", "New American"]
+    when "Southern"
+      categories = ["Southern", "Soul Food"]
+    when "Food  Truck"
+      categories = ["Food Truck"]
+    when "Greek"
+      categories = ["Greek"]
+    when "Middle Eastern"
+      categories = ["Turkish", "Lebanese", "Falafel"]
+    when "Fast Food"
+      categories = ["Fast Food", "Fried Chicken"]
     end
 
-    results = []
+    @results = []
 
     categories.each do |category|
       if Cuisine.find_by(:name => category)
-        results << Cuisine.find_by(:name => category).restaurants
+        @results << Cuisine.find_by(:name => category).restaurants
       end
     end
 
-    results
+    @results.flatten
+
+  end
+
+  def get_coordinates(array)
+    array.collect do |restaurant|
+      [restaurant[:latitude], restaurant[:longitude]]
+    end
   end
 
 end
