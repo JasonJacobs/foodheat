68b32179fb4b1231fb6f271259b0cd3a9cf02830
jeremysklarsky <jeremy.sklarsky@gmail.com>
2015-04-01 15:02:36 -0400

    get pin to work

---
+++ app/assets/javascripts/dropPins.js
@@ -0,0 +1,33 @@
+// $(function(){
+//   dropPin()
+// });
+// // function initialize() {
+// //   debugger
+// //     var locations = [
+// //         ['1', 40.6733818054199, -73.9859237670898, 1],
+// //         ['2', 40.716119, -73.998191, 2]
+
+// //     ];
+
+// //     var mapOptions = {
+// //         center: new google.maps.LatLng(6733818054199, -73.9859237670898),
+// //         zoom: 11,
+// //         mapTypeControl: false,
+// //         panControl: false,
+// //         streetViewControl: false,
+// //         zoomControl: false,
+// //         disableDoubleClickZoom: true,
+// //     };
+// //     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+// //     var marker, i;
+
+// //     for(i=0; i < locations.length; i++){
+// //         marker = new google.maps.Marker({
+// //         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+// //         map: map
+// //     });
+// //   }
+// // }
+
+// marker.setMap(map);
