36a2d64fe6a9ac3058a46b97df341404b1897214
github username <github email address>
2015-04-03 16:18:10 -0400

    addressed merge conflict in search.rb

--- app/models/search.rb
+++ app/models/search.rb
@@ -38,74 +38,9 @@ class Search
     @cuisine = cuisine
   end
 
-<<<<<<< HEAD
   def cuisines
     CUISINES
   end
-=======
-  def search_for_cuisine
-
-    case @cuisine
-    when "Japanese"
-      categories = ["Japanese", "Sushi", "Ramen / Noodles", "Udon", "Sake Bar"]
-    when "Chinese"
-      categories = ["Chinese", "Cantonese", "Dumplings", "Shanghai", "Szechuan"] 
-    when "Mexican"
-      categories = ["Mexican", "Tacos", "Taqueria"]
-    when "Coffee"
-      categories = ["Coffee", "Café"]
-    # when "Bakery"
-    #   categories = ["Bakery", "Bagels"]
-    when "BBQ"
-      categories = ["BBQ"]
-    # when "Fried Chicken"
-    #   categories = ["Fried Chicken", "Wings"]
-    when "Pizza"
-      categories = ["Pizza"]
-    when "Caribbean"
-      categories = ["Caribbean", "Jamaican"]
-    when "Italian"
-      categories = ["Italian"]
-    when "Thai"
-      categories = ["Thai"]
-    when "Indian"
-      categories = ["Indian"]
-    when "French"
-      categories = ["French"]
-    when "American"
-      categories = ["Diner", "American", "New American"]
-    when "Southern"
-      categories = ["Southern", "Soul Food", "Southern / Soul"]
-    when "Food Truck"
-      categories = ["Food Truck"]
-    when "Greek"
-      categories = ["Greek"]
-    when "Middle Eastern"
-      categories = ["Turkish", "Lebanese", "Falafel", "Egyptian"]
-    # when "Fast Food"
-    #   categories = ["Fast Food", "Fried Chicken"]
-    when "Kosher"
-      categories = ["Kosher", "Kosher1", "Jewish"]
-    when "Russian"
-      categories = ["Russian", "Ukrainian"] 
-    when "Polish"
-      categories = ["Polish"] 
-    when "Vegetarian"
-      categories = ["Vegetarian / Vegan", "Vegetarian", "Vegan"]
-    when "Korean"
-      categories = ["Korean"]
-    when "Latin American"
-      categories = ["Latin American", "Peruvian", "Cuban", "Dominican", "Mexican"]
-    when "Bodega"
-      categories = ["Deli / Bodega"]
-    when "African"
-      categories = ["African", "Ethiopian"]
-    when "Filipino"
-      categories = ["Filipino"]
-    end
-
-    @results = []
->>>>>>> d9ba8a7698dabf4a0770fdc43f2aba9821c08903
 
   def search_for_cuisine 
     cuisines[cuisine].flat_map do |category|
